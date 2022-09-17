cf436ea1ff914490cf1a02b6d6d5850124d103dc
dramos777 <83480629+dramos777@users.noreply.github.com>
2022-09-16 22:56:21 -0300

    Update cert-create.sh

--- cert-create.sh
+++ cert-create.sh
@@ -2,3 +2,4 @@
 #Create files.pem needed for webserver
 
 openssl req -newkey rsa:2048 -nodes -keyout ./certificates/privkey.pem -x509 -days 365 -out ./certificates/fullchain.pem && openssl dhparam -out ./certificates/dhparam.pem 2048
+chmod -w ./certificates/*
