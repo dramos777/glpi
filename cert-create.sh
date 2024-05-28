ad50eee6bf64efb86a6611c8655e976f8bbef844
dramos <83480629+dramos777@users.noreply.github.com>
2024-05-28 11:14:08 -0300

    Added variables on file cert-create.sh

--- cert-create.sh
+++ cert-create.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 #Create files.pem needed for webserver
 
-openssl req -newkey rsa:2048 -nodes -keyout ./certificates/privkey.pem -x509 -days 365 -out ./certificates/fullchain.pem && openssl dhparam -out ./certificates/dhparam.pem 2048
-chmod -w ./certificates/*
+CERTDIR="./certificates/"
+KEYNAME="privkey.pem"
+CERTNAME="fullchain.pem"
+DHNAME="dhparam.pem"
+VALID="365"
+
+#Create Certificates
+openssl req -newkey rsa:2048 -nodes -keyout "$CERTDIR$KEYNAME" -x509 -days "$VALID" -out "$CERTDIR$CERTNAME" && openssl dhparam -out "$CERTDIR$DHNAME" 2048
+
+#Remove write permission on files
+chmod -w $CERTDIR*
