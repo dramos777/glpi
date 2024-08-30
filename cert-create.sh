979d1467b5bed93730a13ebc8d4ec4c7c7b9ef25
Emanuel Dramos <emanuel.dramos2@gmail.com>
2024-08-30 16:50:05 -0300

    Add certbot - reconfigure nginx

--- cert-create.sh
+++ cert-create.sh
@@ -4,11 +4,10 @@
 CERTDIR="./certificates/"
 KEYNAME="privkey.pem"
 CERTNAME="fullchain.pem"
-DHNAME="dhparam.pem"
-VALID="365"
+VALID="1095"
 
 #Create Certificates
-openssl req -newkey rsa:2048 -nodes -keyout "$CERTDIR$KEYNAME" -x509 -days "$VALID" -out "$CERTDIR$CERTNAME" && openssl dhparam -out "$CERTDIR$DHNAME" 2048
+openssl req -newkey rsa:4096 -nodes -keyout "$CERTDIR$KEYNAME" -x509 -days "$VALID" -out "$CERTDIR$CERTNAME"
 
 #Remove write permission on files
 chmod -w $CERTDIR*
